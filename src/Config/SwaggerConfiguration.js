874ccdfb220ca70c1d26a9a5c213ed36a537d459
Ryan Lemes <ryanlemes@Ryans-MacBook-Pro.local>
2020-05-01 19:19:38 -0300

    Swagger definitions

---
+++ src/Config/SwaggerConfiguration.js
@@ -0,0 +1,19 @@
+// https://swagger.io/specification/#infoObject
+const swaggerOptions = {
+  swaggerDefinition: {
+    info: {
+      title: 'Calibration backend',
+      description: 'Backend api for Calibrator application for PUC tcc',
+      contact: {
+        name: 'Lidiane Cássia & Ryan Lemes',
+        email: 'ryan.bezerra@sga.pucminas.br',
+      },
+      servers: ['http://localhost:3333'],
+      version: '1.0.0',
+    },
+  },
+  apis: ['./src/Controllers/*.js'],
+};
+
+
+module.exports = swaggerOptions;
