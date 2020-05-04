3a41e75d2b9af6418a6e8643c57356ea2461ac8a
Ryan Lemes <ryanlemes@Ryans-MacBook-Pro.local>
2020-05-04 02:10:04 -0300

    Implement login method and logic

--- src/Controllers/AuthController.js
+++ src/Controllers/AuthController.js
@@ -1,4 +1,8 @@
 const HttpStatus = require('http-status-codes');
+const bcrypt = require('bcrypt');
+
+const generateToken = require('../Config/Authentication');
+const User = require('../Models/User');
 
 module.exports = {
   /**
@@ -16,6 +20,26 @@ module.exports = {
    *         description: Internal server error.
    */
   async login(req, res) {
-    return res.status(HttpStatus.OK);
+    const { username, password } = req.body;
+
+    const user = await User.findOne({ username }).select('+password');
+
+    if (!user) {
+      return res
+        .status(HttpStatus.NOT_FOUND)
+        .json({ message: 'User not found' });
+    }
+
+    if (!await bcrypt.compare(password, user.password)) {
+      return res
+        .status(HttpStatus.UNAUTHORIZED)
+        .json({ message: 'Invalid password' });
+    }
+
+    user.password = undefined;
+
+    return res
+      .status(HttpStatus.OK)
+      .json({ user, token: generateToken({ id: user.id }) });
   },
 };
