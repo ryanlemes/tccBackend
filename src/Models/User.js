9c3b3bd310cd602b8d8fc6a36cf2cdcae11d0a71
Ryan Lemes <ryanlemes@Ryans-MacBook-Pro.local>
2020-05-04 02:08:40 -0300

    Update user model set requirement for properties.

--- src/Models/User.js
+++ src/Models/User.js
@@ -1,11 +1,29 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  name: String,
-  cpf: String,
+  name: {
+    type: String,
+    required: true,
+  },
+  cpf: {
+    type: String,
+    required: true,
+  },
   address: String,
-  username: String,
-  email: String,
+  username: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    select: false,
+  },
+  email: {
+    type: String,
+    required: true,
+    lowercase: true,
+  },
   picture: String,
   lastAccess: {
     type: Date,
